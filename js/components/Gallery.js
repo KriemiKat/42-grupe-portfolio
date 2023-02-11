cd1017d97c58f462f160881d860b76172b945e8a
Katažina <katazina.fukalova@gmail.com>
2023-02-11 23:12:53 +0200

    JS first project

---
+++ js/components/Gallery.js
@@ -0,0 +1,194 @@
+class Gallery {
+    constructor(selector, data) {
+        this.selector = selector;
+        this.data = data;
+
+        this.imgFolder = '';
+        this.contentOrder = 'default';
+        this.size = {
+            min: 1,
+            max: 12,
+        };
+        this.DOM = null;
+
+        this.init();
+    }
+
+    init() {
+        if (!this.isValidSelector() || !this.isValidData()) {
+            return false;
+        }
+
+        this.render();
+        this.enableFilter();
+    }
+
+    isValidSelector() {
+        try {
+            this.DOM = document.querySelector(this.selector);
+            return !!this.DOM;
+        } catch (error) {
+            return false;
+        }
+    }
+
+    // isValidSelectorOldschool() {
+    //     if (typeof this.selector !== 'string' || this.selector === '') {
+    //         return false;
+    //     }
+
+    //     this.DOM = document.querySelector(this.selector);
+    //     return !!this.DOM;
+    // }
+
+    isNonEmptyString(str) {
+        return typeof str === 'string' && str.trim() !== '';
+    }
+
+    isPositiveInteger(num) {
+        return Number.isInteger(num) && num > 0;
+    }
+
+    isNonEmptyArray(arr, minSize = 1) {
+        return Array.isArray(arr) && arr.length >= minSize;
+    }
+
+    isValidData() {
+        const { imgFolder, contentOrder, size, data } = this.data ?? {};
+        const { min, max } = size ?? {};
+
+        if (this.isNonEmptyString(imgFolder)) {
+            this.imgFolder = imgFolder.trim();
+        }
+
+        if (this.isNonEmptyString(contentOrder)) {
+            this.contentOrder = contentOrder.trim();
+        }
+
+        if (size && this.isPositiveInteger(min)) {
+            this.size.min = min;
+        }
+
+        if (size && this.isPositiveInteger(max)) {
+            this.size.max = max;
+        }
+
+        if (!this.isNonEmptyArray(data, this.size.min)) {
+            return false;
+        }
+
+        const correctData = [];
+
+        for (const item of data) {
+            if (
+                typeof item !== 'object' ||
+                Array.isArray(item) ||
+                item === null
+            ) {
+                continue;
+            }
+
+            const { img, alt, title, href, tags } = item;
+
+            if (
+                !this.isNonEmptyString(img) ||
+                !this.isNonEmptyString(alt) ||
+                !this.isNonEmptyString(title) ||
+                !this.isNonEmptyString(href) ||
+                !this.isNonEmptyArray(tags)
+            ) {
+                continue;
+            }
+
+            item.img = item.img.trim();
+            item.alt = item.alt.trim();
+            item.title = item.title.trim();
+            item.href = item.href.trim();
+
+            const validTags = [];
+            for (const tag of tags) {
+                if (this.isNonEmptyString(tag)) {
+                    validTags.push(tag.trim());
+                }
+            }
+
+            if (validTags.length === 0) {
+                continue;
+            }
+
+            item.tags = validTags;
+
+            correctData.push(item);
+        }
+
+        this.data.data = correctData;
+
+        return true;
+    }
+
+    filterHTML() {
+        const data = this.data.data.map((item) => item.tags);
+
+        // const tagsList = [];
+        // for (const tags of data) {
+        //     for (const tag of tags) {
+        //         tagsList.push(tag);
+        //     }
+        // }
+        const tagsList = data.flat();
+
+        const uniqueTags = [];
+        for (const tag of tagsList) {
+            if (!uniqueTags.includes(tag)) {
+                uniqueTags.push(tag);
+            }
+        }
+
+        let HTML = '<div class="option active">All</div>';
+
+        for (const tag of uniqueTags) {
+            HTML += `<div class="option">${tag}</div>`;
+        }
+
+        return HTML;
+    }
+
+    contentHTML() {
+        let HTML = '';
+
+        let count = 0;
+        for (const item of this.data.data) {
+            if (count >= this.size.max) {
+                break;
+            }
+            count++;
+            const path = this.imgFolder + item.img;
+
+            HTML += `<div class="card">
+                        <img class="image" src="${path}" alt="${item.alt}">
+                        <a class="title" href="${item.href}">${item.title}</a>
+                        <p class="tag">${item.tags[0]}</p>
+                    </div>`;
+        }
+
+        return HTML;
+    }
+
+    render() {
+        const HTML = `<div class="filter">
+                        ${this.filterHTML()}
+                    </div>
+                    <div class="gallery-content">
+                        ${this.contentHTML()}
+                    </div>`;
+
+        this.DOM.classList.add('gallery');
+        this.DOM.innerHTML = HTML;
+    }
+
+    enableFilter() {
+        console.log('ENABLE FILTER....');
+    }
+}
+
+export { Gallery };
---
+++ js/pages/home.js
@@ -0,0 +1,58 @@
+// IMPORT
+import { renderServiceFeatures } from '../components/renderServiceFeatures.js';
+import serviceFeaturesData from '../data/serviceFeaturesData.js';
+
+import { portfolioData } from '../data/portfolioData.js';
+import { Gallery } from '../components/Gallery.js';
+
+// EXECUTION
+
+/* gallery: start */
+new Gallery('#portfolio_block', portfolioData);
+/* gallery: end */
+
+/* header: start */
+const hamburgerDOM = document.querySelector('.hamburger');
+
+function kaDaryti() {
+    console.log('DAROM 2023!!!');
+}
+hamburgerDOM.addEventListener('click', kaDaryti);
+/* header: end */
+
+/* hero: start */
+/* hero: end */
+
+/* service-features: start */
+renderServiceFeatures('features_list', serviceFeaturesData);
+/* service-features: end */
+
+/* features: start */
+/* features: end */
+
+/* clients: start */
+/* clients: end */
+
+/* beautiful: start */
+/* beautiful: end */
+
+/* devices: start */
+/* devices: end */
+
+/* testimonials: start */
+/* testimonials: end */
+
+/* achievements: start */
+/* achievements: end */
+
+/* pricing: start */
+/* pricing: end */
+
+/* faq: start */
+/* faq: end */
+
+/* cta: start */
+/* cta: end */
+
+/* footer: start */
+/* footer: end */
